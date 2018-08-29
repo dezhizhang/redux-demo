466d59e1583aa81584518f29131dc06a07a00b93
张德志 <zhangdezhi@zhangdezhideMacBook-Air.local>
2018-08-29 23:31:38 +0800

    完善功能

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { increment,decrement} from './actions/index'
 import './App.css';
 
 class App extends Component {
@@ -9,8 +10,8 @@ class App extends Component {
       <div className="App">
         <div className='container'>
           <h1>{this.props.counter}</h1>
-          <button onClick={()=>this.props.dispatch({type:'INCREMENT'})} className='btn btn-primary'>INCREMENT</button>
-          <button onClick={()=>this.props.dispatch({type:'DECREMENT'})} className='btn btn-info'>DECREMENT</button>
+          <button onClick={()=>this.props.dispatch(increment())} className='btn btn-primary'>INCREMENT</button>
+          <button onClick={()=>this.props.dispatch(decrement())} className='btn btn-info'>DECREMENT</button>
         </div>
       </div>
     );
