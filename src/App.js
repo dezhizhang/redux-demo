8823ded43258c173da22598acca9cb93b56bb59b
张德志 <zhangdezhi@zhangdezhideMacBook-Air.local>
2018-08-29 23:43:48 +0800

    全部完成所有项止

--- src/App.js
+++ src/App.js
@@ -10,28 +10,24 @@ class App extends Component {
       <div className="App">
         <div className='container'>
           <h1>{this.props.counter}</h1>
-          <button onClick={()=>this.props.dispatch(increment())} className='btn btn-primary'>INCREMENT</button>
-          <button onClick={()=>this.props.dispatch(decrement())} className='btn btn-info'>DECREMENT</button>
+          <button onClick={()=>this.props.increment()} className='btn btn-primary'>INCREMENT</button>
+          <button onClick={()=>this.props.decrement()} className='btn btn-info'>DECREMENT</button>
         </div>
       </div>
     );
   }
 }
 
+App.propTypes={
+  counter:PropTypes.number.isRequired,
+  increment:PropTypes.func.isRequired,
+  decrement:PropTypes.func.isRequired
+}
 
-
-
-
-const mapStateToProps=(state)=>{
-  console.log(state);
-
+const mapStateToProps = (state)=>{
   return {
     counter:state.counter
   }
 }
 
-
-
-
-
-export default connect(mapStateToProps)(App)
+export default connect(mapStateToProps,{increment,decrement})(App)
