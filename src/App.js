16f7dfcf4038c5c63f970881449d6b5596e34514
张德志 <zhangdezhi@zhangdezhideMacBook-Air.local>
2018-08-29 22:47:48 +0800

    redux实现功能

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
+        <div className='container'>
+          <h1>{this.props.value}</h1>
+          <button onClick={this.props.onIncrement} className='btn btn-primary'>INCREMENT</button>
+          <button onClick={this.props.onDecrement} className='btn btn-info'>DECREMENT</button>
+        </div>
       </div>
     );
   }
+}
+
+App.propTypes={
+  value:PropTypes.number.isRequired,
+  onIncrement:PropTypes.func.isRequired,
+  onDecrement:PropTypes.func.isRequired
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import counter from './reducers/counter'
 import registerServiceWorker from './registerServiceWorker';
+const store = createStore(counter);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const render=()=>{
+    ReactDOM.render(<App 
+        value={store.getState()}
+        onDecrement={()=>store.dispatch({type:"DECREMENT"})}
+        onIncrement={()=>store.dispatch({type:'INCREMENT'})}/>, document.getElementById('root'));
+}
+
+render();
+store.subscribe(render);
+
+
+
 registerServiceWorker();
